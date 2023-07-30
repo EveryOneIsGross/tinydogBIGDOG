0fa8ad5b0d674415012dca66ff9b58ab2fe22a03
everyoneisgross <fraser.austin@gmail.com>
2023-07-31 04:21:39 +1200

    Update tinydogBIGDOG.py
    added more memory context using keywords

--- tinydogBIGDOG.py
+++ tinydogBIGDOG.py
@@ -8,17 +8,24 @@ import dotenv
 import pandas as pd
 import pickle
 
-# You'll need to set your own .env file
 dotenv.load_dotenv()
 
 class GPT4AllChatbot:
     def __init__(self):
-        self.model = GPT4All(model_name='yr_local_model.bin')
+        self.model = GPT4All(
+            model_name=('c:\\AI_MODELS\\llama2_7b_chat_uncensored.ggmlv3.q4_0.bin')
+        )
+
         self.embedder = Embed4All()
 
     def generate_response(self, query):
         with self.model.chat_session():
-            return self.model.generate(prompt=query, top_k=1)
+            return self.model.generate(
+                prompt='You are locally hosted query chatbot. Do not write lists, answer as directly as possible.' + query,
+                top_k=1,
+                n_predict=500,
+                temp=0.8
+                )
 
     def embed(self, text):
         return self.embedder.embed(text)
@@ -32,10 +39,15 @@ class OpenAIChatbot:
     def generate_response(self, query):
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
+            temperature=0.7,
+            max_tokens=200,
             messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "system", "content": "You are cloud hosted query chatbot. Do not write lists, answer as directly as possible."},
                 {"role": "user", "content": query}
+            
+
             ]
+            
         )
         return response['choices'][0]['message']['content']
 
@@ -45,10 +57,10 @@ gpt4all_chatbot = GPT4AllChatbot()
 openai_chatbot = OpenAIChatbot()
 
 # Set the threshold for the semantic search
-semantic_search_threshold = 0.3
+semantic_search_threshold = 0.4
 
 # Set the threshold for the cosine similarity score between the query and the chatbot's response
-response_similarity_threshold = 0.6
+response_similarity_threshold = 0.8
 
 # Initialize the chat history and embeddings
 chat_history = []
@@ -61,6 +73,28 @@ if os.path.exists('embeddings.pkl'):
 else:
     # If it doesn't exist, create a new list
     chat_history_embeddings = []
+    
+def recall_memory(query_keywords):
+    # Initialize a counter for recalled conversations
+    recalled_conversations = 0
+    recalled_memory = ""
+
+    # Iterate over the conversation DataFrame
+    for index, row in conversation_df.iterrows():
+        # If the query keywords overlap with the user keywords in the row
+        if set(query_keywords).intersection(set(row['User_Keywords'])):
+            # Append the past conversation to the recalled memory
+            recalled_memory += f"{row['User_Input']} {row['Bot_Response']}\n"
+
+            # Increment the counter
+            recalled_conversations += 1
+
+            # If we've recalled two conversations, stop recalling
+            if recalled_conversations >= 2:
+                break 
+
+    return recalled_memory
+
 
 def semantic_search(query_embedding, history_embeddings):
     # Compute cosine similarity between the query embedding and each history embedding
@@ -90,6 +124,16 @@ def get_response(query):
         similar_response = semantic_search(query_embedding, np.array(chat_history_embeddings))
         if similar_response >= semantic_search_threshold:
             query += ' ' + similar_response
+
+    # Extract keywords from the query
+    query_keywords = TextBlob(query).noun_phrases
+
+    # Recall memory based on the query keywords
+    recalled_memory = recall_memory(query_keywords)
+
+    # Add recalled memory to the prompt
+    if recalled_memory:
+        query += ' ' + recalled_memory
 
     gpt4all_response = gpt4all_chatbot.generate_response(query)
     gpt4all_response_embedding = np.array(gpt4all_chatbot.embed(gpt4all_response)).reshape(1, -1)
@@ -126,18 +170,31 @@ def get_response(query):
 
     return response
 
+
+# Initialize the last response
+last_response = None
+
+# Initialize the chat history embeddings
+chat_history_embeddings = []
+
+# Initialize the conversation DataFrame
+conversation_df = pd.DataFrame(columns=['User_Input', 'Bot_Response', 'User_Sentiment', 'Bot_Sentiment', 'User_Keywords', 'Bot_Keywords', 'Model_Used'])
+
 # Start the conversation loop
 while True:
-    # Prompt the user for a query
-    query = input("Please enter your query: ")
-    
-    # If the user types "bye", end the conversation
-    if query.lower() == "bye":
-        print("Goodbye!")
+    # Get the user's input
+    query = input("You: ")
+
+    # If the user wants to quit, break the loop
+    if query.lower() in ['quit', 'exit', 'stop', 'bye', 'goodbye', 'end']:
         break
 
-    # Otherwise, get and print the response
-    print(get_response(query))
+    # Get the bot's response
+    response = get_response(query)
+
+    # Print the bot's response
+    print(f"Bot: {response}")
+
 
 # Save the conversation to a JSON file
 conversation_df.to_json('conversation.json', orient='records', lines=True, mode='a')
